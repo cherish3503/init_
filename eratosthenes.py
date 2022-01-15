4553260d1b4f29bc51404b26d98e53fe96ff8691
cherish3503 <90964197+cherish3503@users.noreply.github.com>
2022-01-15 15:14:15 +0900

    Create eratosthenes.py

---
+++ eratosthenes.py
@@ -0,0 +1,8 @@
+def eratosthenes(n):
+  prime = [True]*(n+1)
+  for i in range(2, math.floor(n ** 0.5)+1):
+    if prime[i] == True:
+      for j in range(2*i, n+1, i):
+        prime[j] = False
+  
+  return [i for i in range(2,n+1) if prime[i] == True]
