42500e6d4623ca2c3389a7508b5c0b5c8390ac06
cherish3503 <90964197+cherish3503@users.noreply.github.com>
2022-01-21 21:45:06 +0900

    Update eratosthenes.py

--- eratosthenes.py
+++ eratosthenes.py
@@ -2,7 +2,7 @@ import math
 
 def eratosthenes(n):
   # n이하의 소수의 리스트를 리턴
-  prime = [True]*(n+1)
+  prime = [False,False] + [True]*(n-1)
   for i in range(2, math.floor(n ** 0.5)+1):
     if prime[i] == True:
       for j in range(2*i, n+1, i):
